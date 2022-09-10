72d7d6000a41df6b45dfc7a1daa65b8cfb00a55c
digitalmoon <freshlife35@yahoo.com>
2022-09-10 00:35:50 -0400

    created pool

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "Fresh3535!",
+    host: "localhost",
+    port: 5432,
+    database: "perntodo"
+})
+
+module.exports = pools;
