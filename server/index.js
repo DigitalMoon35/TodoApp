e455d75893771ae5ec0dc067fba36e5d60f5d4dc
digitalmoon <freshlife35@yahoo.com>
2022-09-10 00:27:19 -0400

    added DB file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(5050, () => {
     console.log("Sever is up on 5050 GOD!!")
