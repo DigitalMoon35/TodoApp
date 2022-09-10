99a36d922c08321d583ac04cca3e0e9dc353a937
digitalmoon <freshlife35@yahoo.com>
2022-09-10 16:45:27 -0400

    added functionality to InputTodo. Successfully saved to DB

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,13 +2,37 @@ import React, { Fragment, useState } from "react";
 
 
 const InputTodo = () => {
-    const [description, setDescription] = useState("Peanut");
+    // component's state
+    const [description, setDescription] = useState("");
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+
+        try {
+            const body = { description };
+            const response = await fetch("http://localhost:5050/todos/", {
+                method: "POST", 
+                headers: {"Content-type" : "application/json"},
+                body: JSON.stringify(body)
+            })
+            console.log(response)
+        } catch (error) {
+            console.error(error.message)        
+        }
+    }
     return (
         <Fragment>
             <h1 className="todo-list">Pern Todo List</h1>
             <div className="form-box">
-            <form>
-                <input type="text" className="todo-input" value={description}/>
+            <form onSubmit={onSubmitForm}>
+                <input 
+                    type="text" 
+                    className="todo-input" 
+                    value={description}
+                    onChange={e => {
+                        setDescription(e.target.value)
+                    }}
+                />
                 <button className="add-button">Add</button>  
             </form>
             </div>
